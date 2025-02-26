8b7738ae74686405e2a383c55d640964165a6044
perpil <97474956+perpil@users.noreply.github.com>
2025-02-26 10:15:53 -0800

    Initial commit

---
+++ test/node-22-repro.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const Node22Repro = require('../lib/node-22-repro-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/node-22-repro-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new Node22Repro.Node22ReproStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
